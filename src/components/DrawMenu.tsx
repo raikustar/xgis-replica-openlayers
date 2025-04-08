58314a72f185d374ae809b58a410c2d185a6aaad
raikustar <raikubmx@gmail.com>
2025-04-08 18:12:15 +0300

    menu toggle button now uses absolute left to toggle

--- src/components/DrawMenu.tsx
+++ src/components/DrawMenu.tsx
@@ -2,16 +2,17 @@ import { useRef, useState} from "react"
 
 function Menu() {
   const menuWindowRef = useRef<HTMLDivElement>(null)
-  const [width, setWidth] = useState<"350px" | "0px">("350px")
+  const [menuWidth, setMenuWidth] = useState<"0px" | "-350px">("0px")
 
   const toggleMenuWindow = () => {
+    // animation keyframes need to be manually adjusted when width changes are made
     if (menuWindowRef.current?.className === "selection_menu") {
-      if (width === "350px") {
+      if (menuWidth === "-350px") {
         menuWindowRef.current.style.animationName = "menu-animation-close"
-        setWidth("0px")
+        setMenuWidth("0px")
       } else {
         menuWindowRef.current.style.animationName = "menu-animation-open"
-        setWidth("350px")
+        setMenuWidth("-350px")
 
       }
     }
@@ -19,8 +20,11 @@ function Menu() {
 
   return (
     <>
-        <div ref={menuWindowRef} className='selection_menu' style={{width:width}}>
+        <div ref={menuWindowRef} className='selection_menu' style={{left:menuWidth}}>
             <button onClick={() => toggleMenuWindow()} className="selection_menu_button flex-c">O</button>
+            <div className="selection_menu_fields">
+
+            </div>
         </div>
     </>
   )
